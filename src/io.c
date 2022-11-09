5ab05f88b03e6529bf9622f8e654879c7c40403c
YackeP <pietracha.jacek@gmail.com>
2022-11-09 18:53:31 +0100

    smol fixes to printing board

--- src/io.c
+++ src/io.c
@@ -1,18 +1,20 @@
-void printBoard(int *board, int ySize, int xSize)
+#include <stdio.h>
+
+void printBoard(int **board, int ySize, int xSize)
 {
   for (int y = 0; y < ySize; y++)
   {
     printf("| ");
     for (int x = 0; x < xSize; x++)
     {
-      int val = board[y * xSize + x];
+      int val = board[y][x];
       if (val == 0)
       {
         printf("-  ");
       }
       else if (val < 0)
       {
-        printf("p%d ", abs(val));
+        printf("p%d ", -val);
       }
       else
       {
