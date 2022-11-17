ca13933900cfb4373fd75ead969572574a1fab21
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2022-11-17 17:24:07 +0100

    implement basic board rendering in the GUI

--- include/gui/main.hh
+++ include/gui/main.hh
@@ -1,19 +1,50 @@
 #pragma once
 
 #include <wx/app.h>
+#include <wx/dc.h>
 #include <wx/frame.h>
+#include <wx/panel.h>
 
-class PenguinsApp : public wxApp {
+extern "C" {
+#include "board.h"
+}
+
+class PenguinsApp;
+
+class BoardPanel : public wxPanel {
 public:
-  virtual bool OnInit();
+  BoardPanel(wxFrame* parent);
+
+  static const wxCoord CELL_SIZE = 32;
+  static const int CELL_FONT_SIZE = 16;
+  static const int FISH_CIRCLE_RADIUS = 4;
+
+private:
+  void OnPaint(wxPaintEvent& event);
+  virtual wxSize DoGetBestClientSize() const override;
 };
 
-class PenguinsFrame : public wxFrame {
+class GameFrame : public wxFrame {
 public:
-  PenguinsFrame();
+  GameFrame();
 
 private:
   void OnHello(wxCommandEvent& event);
   void OnExit(wxCommandEvent& event);
   void OnAbout(wxCommandEvent& event);
+
+  BoardPanel* board_panel;
+};
+
+class PenguinsApp : public wxApp {
+public:
+  PenguinsApp();
+  virtual ~PenguinsApp();
+  virtual bool OnInit() override;
+
+  bool board_inited = false;
+  Board board;
+
+private:
+  GameFrame* game_frame;
 };
--- src/gui/main.cc
+++ src/gui/main.cc
@@ -1,51 +1,152 @@
 #include "gui/main.hh"
+#include <cmath>
+#include <wx/dcclient.h>
+#include <wx/gdicmn.h>
+#include <wx/graphics.h>
+#include <wx/gtk/brush.h>
 #include <wx/log.h>
 #include <wx/menu.h>
 #include <wx/msgdlg.h>
+#include <wx/sizer.h>
 
-enum { ID_Hello = 1 };
+extern "C" {
+#include "utils.h"
+}
 
 wxIMPLEMENT_APP(PenguinsApp);
 
+PenguinsApp::PenguinsApp() {}
+
+PenguinsApp::~PenguinsApp() {
+  if (this->board_inited) {
+    free_board(&this->board);
+  }
+}
+
 bool PenguinsApp::OnInit() {
-  PenguinsFrame* frame = new PenguinsFrame();
-  frame->Show(true);
+  random_init();
+
+  this->board = init_board(20, 20);
+  this->board_inited = true;
+  generate_random_board(&this->board);
+
+  for (int player = 1; player <= 3; player++) {
+    for (int penguin = 0; penguin < 2; penguin++) {
+      int y = random_range(0, this->board.height - 1);
+      int x = random_range(0, this->board.width - 1);
+      this->board.grid[y][x] = -player;
+    }
+  }
+
+  this->game_frame = new GameFrame();
+  this->game_frame->Show(true);
   return true;
 }
 
-PenguinsFrame::PenguinsFrame() : wxFrame(nullptr, wxID_ANY, "Hello World") {
-  wxMenu* menuFile = new wxMenu();
-  menuFile->Append(
-    ID_Hello, "&Hello...\tCtrl-H", "Help string shown in status bar for this menu item");
-  menuFile->AppendSeparator();
+GameFrame::GameFrame() : wxFrame(nullptr, wxID_ANY, "Penguins game") {
+  auto menuFile = new wxMenu();
   menuFile->Append(wxID_EXIT);
 
-  wxMenu* menuHelp = new wxMenu();
+  auto menuHelp = new wxMenu();
   menuHelp->Append(wxID_ABOUT);
 
-  wxMenuBar* menuBar = new wxMenuBar();
+  auto menuBar = new wxMenuBar();
   menuBar->Append(menuFile, "&File");
   menuBar->Append(menuHelp, "&Help");
 
+  auto v_sizer = new wxBoxSizer(wxVERTICAL);
+  auto h_sizer = new wxBoxSizer(wxHORIZONTAL);
+
+  this->board_panel = new BoardPanel(this);
+  v_sizer->Add(board_panel, 1, wxALIGN_CENTRE);
+  h_sizer->Add(v_sizer, 1, wxALIGN_CENTRE);
+
+  this->SetSizerAndFit(h_sizer);
   this->SetMenuBar(menuBar);
 
   this->CreateStatusBar();
   this->SetStatusText("Welcome to wxWidgets!");
 
-  this->Bind(wxEVT_MENU, &PenguinsFrame::OnHello, this, ID_Hello);
-  this->Bind(wxEVT_MENU, &PenguinsFrame::OnAbout, this, wxID_ABOUT);
-  this->Bind(wxEVT_MENU, &PenguinsFrame::OnExit, this, wxID_EXIT);
+  this->Bind(wxEVT_MENU, &GameFrame::OnAbout, this, wxID_ABOUT);
+  this->Bind(wxEVT_MENU, &GameFrame::OnExit, this, wxID_EXIT);
 }
 
-void PenguinsFrame::OnExit(wxCommandEvent& event) {
+void GameFrame::OnExit(wxCommandEvent& event) {
   this->Close(true);
 }
 
-void PenguinsFrame::OnAbout(wxCommandEvent& event) {
+void GameFrame::OnAbout(wxCommandEvent& event) {
   wxMessageBox(
-    "This is a wxWidgets Hello World example", "About Hello World", wxOK | wxICON_INFORMATION);
+    "This is a wxWidgets Hello World example", "About Hello World", wxOK | wxICON_INFORMATION
+  );
 }
 
-void PenguinsFrame::OnHello(wxCommandEvent& event) {
-  wxLogMessage("Hello world from wxWidgets!");
+BoardPanel::BoardPanel(wxFrame* parent) : wxPanel(parent) {
+  this->Bind(wxEVT_PAINT, &BoardPanel::OnPaint, this);
+  this->SetBackgroundColour(*wxWHITE);
+}
+
+wxSize BoardPanel::DoGetBestClientSize() const {
+  auto board = &wxGetApp().board;
+  return CELL_SIZE * wxSize(board->width, board->height);
+}
+
+void BoardPanel::OnPaint(wxPaintEvent& event) {
+  wxPaintDC dc(this);
+
+  dc.SetTextForeground(*wxBLACK);
+  const wxFont default_font = dc.GetFont();
+  wxFont cell_font(default_font);
+  cell_font.MakeBold();
+  cell_font.SetPointSize(CELL_FONT_SIZE);
+  dc.SetFont(cell_font);
+
+  auto board = &wxGetApp().board;
+  for (int y = 0; y < board->height; y++) {
+    for (int x = 0; x < board->width; x++) {
+      int cell = board->grid[y][x];
+      wxRect cell_rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+      wxPoint cell_centre = cell_rect.GetPosition() + cell_rect.GetSize() / 2;
+
+      dc.SetPen(*wxBLACK_PEN);
+
+      if (cell > 0) {
+        // an ice floe with fish on it
+        int fish_count = cell;
+        dc.SetBrush(*wxWHITE_BRUSH);
+        dc.DrawRectangle(cell_rect);
+
+        dc.SetPen(wxNullPen);
+        dc.SetBrush(*wxGREY_BRUSH);
+        if (fish_count == 1) {
+          dc.DrawCircle(cell_centre, FISH_CIRCLE_RADIUS);
+        } else {
+          for (int i = 0; i < fish_count; i++) {
+            float angle = (float)i / fish_count * 2 * M_PI;
+            float offset = (float)CELL_SIZE / 5;
+            wxPoint centre(
+              (float)cell_centre.x + cos(angle) * offset,
+              (float)cell_centre.y + sin(angle) * offset
+            );
+            dc.DrawCircle(centre, FISH_CIRCLE_RADIUS);
+          }
+        }
+
+      } else if (cell < 0) {
+        // a penguin
+        int player = -cell;
+        dc.SetBrush(*wxYELLOW_BRUSH);
+        dc.DrawRectangle(cell_rect);
+
+        wxString str;
+        str << player;
+        dc.DrawLabel(str, cell_rect, wxALIGN_CENTRE);
+
+      } else {
+        // a water tile
+        dc.SetBrush(*wxCYAN_BRUSH);
+        dc.DrawRectangle(cell_rect);
+      }
+    }
+  }
 }
