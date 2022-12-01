138f1d2e64b2f2859ad3d517cc189d1711859085
YackeP <pietracha.jacek@gmail.com>
2022-12-01 14:59:13 +0100

    Link project files to the test library Add munit-specific implementation of random to the test file to replace random_range from random.h

--- readme.md
+++ readme.md
@@ -15,3 +15,7 @@ To download it for the first time, run
 `git submodule init && git submodule update && make cmake`
 
 then, use `make test` to run all the tests in tests.c
+
+### Rerunning tests with a predetermined randomness seed
+
+`make test && build/penguins-tests --seed 0xbf8782eb`
--- src/tests.c
+++ src/tests.c
@@ -4,10 +4,63 @@
 #include <munit.h>
 #include <stdio.h>
 
+#include "board.h"
+#include "random.h"
+
+// an implementation of the function defined in random.h and used in board.c
+int random_range(int min, int max) {
+  return munit_rand_int_range(min, max);
+}
+
 static MunitResult test_example(const MunitParameter params[], void* data) {
   munit_assert(2 + 2 == 4);
+  munit_assert(random_range(0, 1) == 0);
   return MUNIT_OK;
 }
+
+static MunitResult test_placeable_spot_exists(const MunitParameter params, void* data) {
+  int grid[2][3] = { { 0, 1, 2 }, { -1, 2, 1 } };
+  int** pointer_grid = calloc(2, sizeof(int*));
+  pointer_grid[0] = grid[0];
+  pointer_grid[1] = grid[1];
+
+  Board board = { .height = 2, .width = 3, .grid = pointer_grid };
+
+  munit_assert_true(placeable_spot_exists(&board));
+  return MUNIT_OK;
+}
+
+static MunitResult test_placeable_spot_does_not_exist(const MunitParameter params, void* data) {
+  int grid[2][3] = { { 0, 0, 2 }, { -1, -2, 1 } };
+  int** pointer_grid = calloc(2, sizeof(int*));
+  pointer_grid[0] = grid[0];
+  pointer_grid[1] = grid[1];
+
+  Board board = { .height = 2, .width = 3, .grid = pointer_grid };
+
+  // munit_assert_false(placeable_spot_exists(&board));
+  return MUNIT_OK;
+}
+
+static MunitTest board_suite_tests[] = {
+  {
+    .name = "/placeable_spot_exists",
+    .test = test_placeable_spot_exists,
+    .setup = NULL,
+    .tear_down = NULL,
+    .options = MUNIT_TEST_OPTION_NONE,
+    .parameters = NULL,
+  },
+  {
+    .name = "/detects that there are no valid placement slots",
+    .test = test_placeable_spot_does_not_exist,
+    .setup = NULL,
+    .tear_down = NULL,
+    .options = MUNIT_TEST_OPTION_NONE,
+    .parameters = NULL,
+  },
+  { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+};
 
 static MunitTest test_suite_tests[] = {
   {
@@ -23,14 +76,22 @@ static MunitTest test_suite_tests[] = {
   { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
 };
 
-static const MunitSuite test_suite = {
-  .prefix = "/penguins",
-  .tests = test_suite_tests,
+static const MunitSuite board_suite = {
+  .prefix = "/board",
+  .tests = board_suite_tests,
   .suites = NULL,
   .iterations = 1,
   .options = MUNIT_SUITE_OPTION_NONE,
 };
 
+// static const MunitSuite test_suite = {
+//   .prefix = "/penguins",
+//   .tests = test_suite_tests,
+//   .suites = board_suite,
+//   .iterations = 1,
+//   .options = MUNIT_SUITE_OPTION_NONE,
+// };
+
 int main(int argc, char* argv[]) {
-  return munit_suite_main(&test_suite, NULL, argc, argv);
+  return munit_suite_main(&board_suite, NULL, argc, argv);
 }
