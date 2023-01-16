0be9fc183dccd750d251d18e12b34967d5d8c42b
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2023-01-16 18:14:39 +0100

    Improve the C random generator
    This is necessary for the autonomous mode since every move is computed
    by starting a whole new process, so the RNG state is not preserved in
    any way, therefore we need a good seed.

--- src/random.c
+++ src/random.c
@@ -1,12 +1,42 @@
 #include "random.h"
 #include <stdlib.h>
-#include <time.h>
 
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#else
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+#endif
+
+// Taken from <https://cplusplus.com/faq/beginners/random-numbers/#seeding>.
 void random_init(void) {
-  srand(time(NULL));
+  unsigned long seed;
+#ifdef _WIN32
+  FILETIME t;
+  ULARGE_INTEGER i;
+  GetSystemTimeAsFileTime(&t);
+  i.u.LowPart = t.dwLowDateTime;
+  i.u.HighPart = t.dwHighDateTime;
+  seed = i.QuadPart / 1000;
+#else
+  struct timespec ts, res;
+  clock_getres(CLOCK_REALTIME, &res);
+  clock_gettime(CLOCK_REALTIME, &ts);
+  seed = ts.tv_nsec / res.tv_nsec;
+#endif
+  srand(seed);
 }
 
-// Taken from <https://stackoverflow.com/a/18386648>
+// Taken from <https://cplusplus.com/faq/beginners/random-numbers/#random_int_in_range>.
 int random_range(int min, int max) {
-  return min + rand() / (RAND_MAX / (max - min + 1) + 1);
+  unsigned int n = (max - min <= RAND_MAX) ? (max - min + 1U) : (RAND_MAX + 1U);
+  unsigned int x = (RAND_MAX + 1U) / n;
+  unsigned int y = x * n;
+  unsigned int r;
+  do {
+    r = rand();
+  } while (r >= y);
+  return r / x + min;
 }
