32a82e056483fe662e408c55b38f77ff9fd84ba8
YackeP <pietracha.jacek@gmail.com>
2023-01-24 06:27:33 +0100

    Update tests to the new form of the methods

--- src/tests.c
+++ src/tests.c
@@ -4,7 +4,9 @@
 
 #include "board.h"
 #include "movement.h"
+#include "placement.h"
 #include "random.h"
+
 #include <munit.h>
 #include <stdio.h>
 
@@ -15,83 +17,192 @@ int random_range(int min, int max) {
 
 static MunitResult
 test_placeable_spot_exists(const MunitParameter* UNUSED(params), void* UNUSED(data)) {
-  int* grid[2] = { (int[3]){ 0, 1, 2 }, (int[3]){ -1, 2, 1 } };
-  Board board = { .height = 2, .width = 3, .grid = grid };
-
-  assert_true(placeable_spot_exists(&board));
+  int grid[] = { 0, 1, 2, 
+                -1, 2, 1 };
+  Game game = { .phase = GAME_PHASE_PLACEMENT,
+                .players = NULL,
+                .players_count = 1,
+                .penguins_per_player = 1,
+                .board_width = 3,
+                .board_height = 2,
+                .board_grid = grid,
+                .current_player_index = 0 };
+  assert_true(any_valid_placement_exists(&game));
   return MUNIT_OK;
 }
 
 static MunitResult
 test_placeable_spot_does_not_exist(const MunitParameter* UNUSED(params), void* UNUSED(data)) {
-  int* grid[2] = { (int[3]){ 0, 0, 2 }, (int[3]){ -1, -2, 3 } };
-  Board board = { .height = 2, .width = 3, .grid = grid };
+  int grid[] = { 0, 0, 2, 
+                -1, -2, 3 };
+  Game game = { .phase = GAME_PHASE_PLACEMENT,
+                .players = NULL,
+                .players_count = 1,
+                .penguins_per_player = 1,
+                .board_width = 3,
+                .board_height = 2,
+                .board_grid = grid,
+                .current_player_index = 0 };
 
-  assert_false(placeable_spot_exists(&board));
+  assert_false(any_valid_placement_exists(&game));
   return MUNIT_OK;
 }
 
 static MunitResult
 test_valid_movement_exists_for_player(const MunitParameter* UNUSED(params), void* UNUSED(data)) {
-  int* grid[2] = { (int[3]){ -1, 0, -2 }, (int[3]){ 0, 1, 3 } };
-  Board board = { .height = 2, .width = 3, .grid = grid };
+  int grid[] = { -2, 0, -1, 
+                0, 1, 3 };
+  Player players[] = { { .id = 1,
+                         .name = "1",
+                         .points = 0,
+                         .penguins_count = 1,
+                         .penguins = &((Coords){ 2, 0 }),
+                         .moves_count = 0 },
+                       { .id = 2,
+                         .name = "2",
+                         .points = 0,
+                         .penguins_count = 1,
+                         .penguins = &((Coords){ 0, 0 }),
+                         .moves_count = 0 } };
+  Game game = { .phase = GAME_PHASE_MOVEMENT,
+                .players = players,
+                .players_count = 2,
+                .penguins_per_player = 1,
+                .board_width = 3,
+                .board_height = 2,
+                .board_grid = grid,
+                .current_player_index = 0 };
 
-  assert_true(any_valid_player_move_exists(&board, 2));
+  assert_true(any_valid_player_move_exists(&game, 0));
   return MUNIT_OK;
 }
 
 static MunitResult test_no_valid_movement_exists_for_player(
   const MunitParameter* UNUSED(params), void* UNUSED(data)
 ) {
-  int* grid[2] = { (int[3]){ -1, 0, -2 }, (int[3]){ 0, 1, 3 } };
-  Board board = { .height = 2, .width = 3, .grid = grid };
+  int grid[] = { -2, 0, -1, 
+                0, 1, 3 };
+  Player players[] = { { .id = 1,
+                         .name = "1",
+                         .points = 0,
+                         .penguins_count = 1,
+                         .penguins = &((Coords){ 2, 0 }),
+                         .moves_count = 0 },
+                       { .id = 2,
+                         .name = "2",
+                         .points = 0,
+                         .penguins_count = 1,
+                         .penguins = &((Coords){ 0, 0 }),
+                         .moves_count = 0 } };
+  Game game = { .phase = GAME_PHASE_MOVEMENT,
+                .players = players,
+                .players_count = 2,
+                .penguins_per_player = 1,
+                .board_width = 3,
+                .board_height = 2,
+                .board_grid = grid,
+                .current_player_index = 1 };
 
-  assert_false(any_valid_player_move_exists(&board, 1));
+  assert_false(any_valid_player_move_exists(&game, 1));
   return MUNIT_OK;
 }
 
 static MunitResult
 test_detect_valid_movement(const MunitParameter* UNUSED(params), void* UNUSED(data)) {
-  int* grid[1] = { (int[3]){ -1, 1, 3 } };
-  Board board = { .height = 1, .width = 3, .grid = grid };
+  int grid[] = { -1, 1, 3 };
+  Player players[] = { { .id = 1,
+                         .name = "1",
+                         .points = 0,
+                         .penguins_count = 1,
+                         .penguins = &((Coords){ 2, 0 }),
+                         .moves_count = 0 } };
+  Game game = { .phase = GAME_PHASE_MOVEMENT,
+                .players = players,
+                .players_count = 1,
+                .penguins_per_player = 1,
+                .board_width = 3,
+                .board_height = 1,
+                .board_grid = grid,
+                .current_player_index = 0 };
+  Coords fail = { -1, -1 };
 
-  assert_true(
-    validate_movement(&board, (Coords){ 0, 0 }, (Coords){ 2, 0 }, 1, NULL) == VALID_INPUT
-  );
+  assert_true(validate_movement(&game, (Coords){ 0, 0 }, (Coords){ 2, 0 }, &fail) == VALID_INPUT);
   return MUNIT_OK;
 }
 
 static MunitResult
 test_movement_over_empty_space_invalid(const MunitParameter* UNUSED(params), void* UNUSED(data)) {
-  int* grid[1] = { (int[3]){ -1, 0, 3 } };
-  Board board = { .height = 1, .width = 3, .grid = grid };
+  int grid[] = { -1, 0, 3 };
+  Player players[] = { { .id = 1,
+                         .name = "1",
+                         .points = 0,
+                         .penguins_count = 1,
+                         .penguins = &((Coords){ 2, 0 }),
+                         .moves_count = 0 } };
+  Game game = { .phase = GAME_PHASE_MOVEMENT,
+                .players = players,
+                .players_count = 1,
+                .penguins_per_player = 1,
+                .board_width = 3,
+                .board_height = 1,
+                .board_grid = grid,
+                .current_player_index = 0 };
+  Coords fail = { -1, -1 };
 
-  assert_false(
-    validate_movement(&board, (Coords){ 0, 0 }, (Coords){ 2, 0 }, 1, NULL) == VALID_INPUT
+  assert_true(
+    validate_movement(&game, (Coords){ 0, 0 }, (Coords){ 2, 0 }, &fail) == MOVE_ONTO_EMPTY_TILE
   );
   return MUNIT_OK;
 }
 
 static MunitResult
 test_movement_over_penguin_invalid(const MunitParameter* UNUSED(params), void* UNUSED(data)) {
-  int* grid[1] = { (int[3]){ -1, -2, 3 } };
-  Board board = { .height = 1, .width = 3, .grid = grid };
+  int* grid[] = { -1, -2, 3 };
+  Player players[] = { { .id = 1,
+                         .name = "1",
+                         .points = 0,
+                         .penguins_count = 1,
+                         .penguins = &((Coords){ 2, 0 }),
+                         .moves_count = 0 } };
+  Game game = { .phase = GAME_PHASE_MOVEMENT,
+                .players = players,
+                .players_count = 1,
+                .penguins_per_player = 1,
+                .board_width = 3,
+                .board_height = 1,
+                .board_grid = grid,
+                .current_player_index = 0 };
+  Coords fail = { -1, -1 };
 
-  assert_false(
-    validate_movement(&board, (Coords){ 0, 0 }, (Coords){ 2, 0 }, 1, NULL) == VALID_INPUT
+  assert_true(
+    validate_movement(&game, (Coords){ 0, 0 }, (Coords){ 2, 0 }, &fail) == MOVE_OVER_PENGUIN
   );
   return MUNIT_OK;
 }
 
 static MunitResult
 test_move_penguin_and_calculate_points(const MunitParameter* UNUSED(params), void* UNUSED(data)) {
-  int* grid[1] = { (int[3]){ -1, 1, 3 } };
-  Board board = { .height = 1, .width = 3, .grid = grid };
+  int* grid[] = { -1, 1, 3 };
+  Player players[] = { { .id = 1,
+                         .name = "1",
+                         .points = 0,
+                         .penguins_count = 1,
+                         .penguins = &((Coords){ 2, 0 }),
+                         .moves_count = 0 } };
+  Game game = { .phase = GAME_PHASE_MOVEMENT,
+                .players = players,
+                .players_count = 1,
+                .penguins_per_player = 1,
+                .board_width = 3,
+                .board_height = 1,
+                .board_grid = grid,
+                .current_player_index = 0 };
+  Coords fail = { -1, -1 };
 
-  assert_int(move_penguin(&board, (Coords){ 0, 0 }, (Coords){ 2, 0 }, 1), ==, 3);
-  assert_int(board.grid[0][0], ==, 0);
-  assert_int(board.grid[0][1], ==, 1);
-  assert_int(board.grid[0][2], ==, -1);
+  assert_int(move_penguin(&game, (Coords){ 0, 0 }, (Coords){ 2, 0 }), ==, 3);
+  assert_int(game.board_grid[0], ==, 0);
+  assert_int(game.board_grid[1], ==, 1);
+  assert_int(game.board_grid[2], ==, -1);
   return MUNIT_OK;
 }
 
