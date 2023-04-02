f4b19caad18006ea138c9455dc2084607d55e38f
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2023-04-03 00:35:35 +0200

    Refactor resource embedding script
    Previously changing a resource file would also trigger a rebuild of
    every source file which #included the header for the resource because
    the header would get regenerated too.

--- src/tools/embed_resource.cc
+++ src/tools/embed_resource.cc
@@ -7,52 +7,68 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-  if (argc != 5) return 1;
-  char* input_path = argv[1];
-  char* output_source_path = argv[2];
-  char* output_header_path = argv[3];
-  char* variable_name = argv[4];
+  if (argc < 2) return 1;
+  char* mode = argv[1];
 
-  std::ifstream input(input_path, std::ios::binary);
-  if (!input) {
-    std::cerr << "open(" << input_path << ") failed: " << strerror(errno) << std::endl;
-    return 1;
-  }
-  std::ofstream out_source(output_source_path);
-  if (!out_source) {
-    std::cerr << "open(" << output_source_path << ") failed: " << strerror(errno) << std::endl;
-    return 1;
-  }
-  std::ofstream out_header(output_header_path);
-  if (!out_header) {
-    std::cerr << "open(" << output_header_path << ") failed: " << strerror(errno) << std::endl;
-    return 1;
-  }
+  if (strcmp(mode, "header") == 0) {
 
-  out_header << "#pragma once" << std::endl;
-  out_header << "#include <stddef.h>" << std::endl;
-  out_header << "#ifdef __cplusplus" << std::endl;
-  out_header << "extern \"C\" {" << std::endl;
-  out_header << "#endif" << std::endl;
-  out_header << "extern const unsigned char " << variable_name << "[];" << std::endl;
-  out_header << "extern const size_t " << variable_name << "_size;" << std::endl;
-  out_header << "#ifdef __cplusplus" << std::endl;
-  out_header << "}" << std::endl;
-  out_header << "#endif" << std::endl;
+    if (argc != 4) return 1;
+    char* output_header_path = argv[2];
+    char* variable_name = argv[3];
 
-  out_source << "#include \"" << output_header_path << "\"" << std::endl;
-  out_source << "const unsigned char " << variable_name << "[] = {";
-  char c;
-  unsigned long long length = 0;
-  while (input.get(c)) {
-    if (length > 0) {
-      out_source << ",";
+    std::ofstream output(output_header_path);
+    if (!output) {
+      std::cerr << "open(" << output_header_path << ") failed: " << strerror(errno) << std::endl;
+      return 1;
     }
-    out_source << (unsigned int)(unsigned char)c;
-    length += 1;
+
+    output << "#pragma once" << std::endl;
+    output << "#include <stddef.h>" << std::endl;
+    output << "#ifdef __cplusplus" << std::endl;
+    output << "extern \"C\" {" << std::endl;
+    output << "#endif" << std::endl;
+    output << "extern const unsigned char " << variable_name << "[];" << std::endl;
+    output << "extern const size_t " << variable_name << "_size;" << std::endl;
+    output << "#ifdef __cplusplus" << std::endl;
+    output << "}" << std::endl;
+    output << "#endif" << std::endl;
+
+  } else if (strcmp(mode, "source") == 0) {
+
+    if (argc != 6) return 1;
+    char* input_path = argv[2];
+    char* output_source_path = argv[3];
+    char* output_header_path = argv[4];
+    char* variable_name = argv[5];
+
+    std::ifstream input(input_path, std::ios::binary);
+    if (!input) {
+      std::cerr << "open(" << input_path << ") failed: " << strerror(errno) << std::endl;
+      return 1;
+    }
+    std::ofstream output(output_source_path);
+    if (!output) {
+      std::cerr << "open(" << output_source_path << ") failed: " << strerror(errno) << std::endl;
+      return 1;
+    }
+
+    output << "#include \"" << output_header_path << "\"" << std::endl;
+    output << "const unsigned char " << variable_name << "[] = {";
+    char c;
+    unsigned long long length = 0;
+    while (input.get(c)) {
+      if (length > 0) {
+        output << ",";
+      }
+      output << (unsigned int)(unsigned char)c;
+      length += 1;
+    }
+    output << "};" << std::endl;
+    output << "const size_t " << variable_name << "_size = " << length << ";" << std::endl;
+
+  } else {
+    return 1;
   }
-  out_source << "};" << std::endl;
-  out_source << "const size_t " << variable_name << "_size = " << length << ";" << std::endl;
 
   return 0;
 }
